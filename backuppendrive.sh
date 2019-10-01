4f260b4de1bb30909982345bfa57600cb7a21c80
DarkSoldierBR <mateus.dias029@gmail.com>
2019-10-01 13:33:36 -0300

    Adicionado notificações

--- backuppendrive.sh
+++ backuppendrive.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 
-##Script simples para backup feito por MATEUS DIAS
+#####################################################
+#|||||||||||||||||||||||||||||||||||||||||||||||||||#
+#|#Script simples para backup feito por MATEUS DIAS|#
+#|||||||||||||||||||||||||||||||||||||||||||||||||||#
+#####################################################
 
-
-#LOG=/var/log/backuppendrive.log
-#exec 1>>${LOG}
-#exec 2>&1
-
-#echo > /var/log/backuppendrive.log
-#date > /var/log/backuppendrive.log
 
 loop=1
 
@@ -17,9 +14,10 @@ while [ $loop = 1 ]
 do
 
 #variaveis
-localbackup="/home/mateus/ETEC/"
-uid="184F39D627BEE814"
-comando=$(lsblk --output UUID,MOUNTPOINT | grep "$uid" | awk '{print $1}')
+localbackup="/home/mateus/ETEC/" #local onde os arquivos do pendrive serão sincronizados
+uid="184F39D627BEE814" #uuid do dispositivo
+comando=$(lsblk --output UUID,MOUNTPOINT | grep "$uid" | awk '{print $1}') 
+adicionar_pasta="ETEC" #pasta na raiz do pendrive que sera sincronizado 
 tempodeverificacao=150   #(em segundos)
 
 
@@ -30,19 +28,27 @@ echo "UUID: $uid"
 #verifica se o pendrive esta conectado
 if [ $comando = $uid ]
 then
+
 echo "Pendrive encontrado"
+$(notify-send "Pendrive encontrado")
 
 mount_point_pendrive=$(lsblk --output UUID,MOUNTPOINT | grep "$uid" | awk '{print $2}')
-caminho=$mount_point_pendrive/ETEC/*
+caminho=$mount_point_pendrive/$adicionar_pasta/*
+
 echo "Caminho do pendrive: $caminho"
-
 echo "Começando a sincronização"
+$(notify-send "Começando a sincronização")
 
-rsync -uahvP $caminho $localbackup
+rsync -uahvP $caminho $localbackup #sincroniza arquivos do pendrive a pasta backup selecionada
+
+$(notify-send "Sincronização finalizada")
 
 sleep $tempodeverificacao
+
 else
+
 echo "Pendrive não encontrado"
+
 sleep $tempodeverificacao
 fi
 
