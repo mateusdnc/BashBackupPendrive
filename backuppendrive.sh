a73052614cac525d548128b869a18c1d5c7c0872
DarkSoldierBR <mateus.dias029@gmail.com>
2019-10-03 21:14:42 -0300

    notificações otimizadas

--- backuppendrive.sh
+++ backuppendrive.sh
@@ -1,50 +1,25 @@
 #!/bin/bash
 
-
 #####################################################
 #|||||||||||||||||||||||||||||||||||||||||||||||||||#
 #|#Script simples para backup feito por MATEUS DIAS|#
 #|||||||||||||||||||||||||||||||||||||||||||||||||||#
 #####################################################
 
-
 loop=1
 
 while [ $loop = 1 ] 
 do
 
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
->>>>>>> notificações otimizadas
 #variaveis configuráveis 
 localbackup="/home/mateus/ETEC/" #local onde os arquivos do pendrive serão sincronizados
 uid="184F39D627BEE814" #uuid do dispositivo
 adicionar_pasta="ETEC" #pasta na raiz do pendrive que sera sincronizado 
-<<<<<<< HEAD
-tempoicacao=250   #(em segundos)
-notificacoes= true #deveriftrue = com notificações,false = o script não enviara notificações.
-=======
 tempodeverificacao=250   #(em segundos)
-notificacoes= true #true = com notificações,false = o script não enviara notificações.
->>>>>>> notificações otimizadas
-
+notificacoes= false #true = com notificações,false = o script não enviara notificações.
 
 #variaveis fixas(não mexer)
 verificapendrive=$(lsblk --output UUID,MOUNTPOINT | grep "$uid" | awk '{print $1}') 
-<<<<<<< HEAD
-=======
-=======
-#variaveis
-localbackup="/home/mateus/ETEC/" #local onde os arquivos do pendrive serão sincronizados
-uid="184F39D627BEE814" #uuid do dispositivo
-comando=$(lsblk --output UUID,MOUNTPOINT | grep "$uid" | awk '{print $1}') 
-adicionar_pasta="ETEC" #pasta na raiz do pendrive que sera sincronizado 
-tempodeverificacao=150   #(em segundos)
->>>>>>> 30ccf38a1e65fd1a9e9cfcd57ea6a0c7173c489d
->>>>>>> notificações otimizadas
-
 
 #confirmando
 echo "Local do backup: $localbackup"
@@ -54,57 +29,41 @@ echo "UUID: $uid"
 if [ $verificapendrive = $uid ]
 then
 
-echo "Pendrive encontrado"
-$(notify-send "Pendrive encontrado")
+	echo "Pendrive encontrado"
+	$(notify-send "Pendrive encontrado")
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 
->>>>>>> notificações otimizadas
-
-#verifica se as notificações estão ativadas
-	if [ $notificacoes = true ]
+	if [ $notificacoes = true ] #verifica se as notificações estão ativadas,se estiver,envia notificações
 	then
 	$(notify-send "Pendrive encontrado")
 	fi 
 
 
-mount_point_pendrive=$(lsblk --output UUID,MOUNTPOINT | grep "$uid" | awk '{print $2}') #localiza onde é o ponto de montagem do dispositivo
-caminho=$mount_point_pendrive/$adicionar_pasta/* #monta um caminho de acordo com as variaveis mount_point_drive e adicionar_pasta
-sincroniza=$(rsync -uahivP --delete $caminho $localbackup)
+	mount_point_pendrive=$(lsblk --output UUID,MOUNTPOINT | grep "$uid" | awk '{print $2}') #localiza onde é o ponto de montagem do dispositivo
+	caminho=$mount_point_pendrive/$adicionar_pasta/* #monta um caminho de acordo com as variaveis mount_point_drive e adicionar_pasta
+	sincroniza=$(rsync -uahivP --delete $caminho $localbackup) #faz as sincronização dos arquivos
 
+	echo "Caminho do pendrive: $caminho"
+	echo "Começando a sincronização"
+		
+	if [ $notificacoes = true ] #verifica se as notificações estão ativadas,se estiver,envia notificações
+	then
+	$(notify-send "Começando a sincronização")
+	fi
 
-<<<<<<< HEAD
+		###########
+		$sincroniza 
+		###########
 
+	if [ $notificacoes = true ]
+	then
+	$(notify-send "Backup Sincronizado")
+	fi 
 
-
-=======
-#informa o caminho do pendrive
-=======
-mount_point_pendrive=$(lsblk --output UUID,MOUNTPOINT | grep "$uid" | awk '{print $2}')
-caminho=$mount_point_pendrive/$adicionar_pasta/*
-
->>>>>>> 30ccf38a1e65fd1a9e9cfcd57ea6a0c7173c489d
-echo "Caminho do pendrive: $caminho"
-echo "Começando a sincronização"
-$(notify-send "Começando a sincronização")
-
-<<<<<<< HEAD
->>>>>>> notificações otimizadas
-		if [ $notificacoes = true ]
-		then
-		$(notify-send "Backup Sincronizado")
-		fi 
-<<<<<<< HEAD
-
-=======
-=======
-rsync -uahvP $caminho $localbackup #sincroniza arquivos do pendrive a pasta backup selecionada
-
-$(notify-send "Sincronização finalizada")
->>>>>>> 30ccf38a1e65fd1a9e9cfcd57ea6a0c7173c489d
->>>>>>> notificações otimizadas
+	if [ $notificacoes = true ] #verifica se as notificações estão ativadas,se estiver,envia notificações
+	then
+	$(notify-send "Sincronização finalizada")
+	fi 
 
 sleep $tempodeverificacao
 
