9ef61acceef0260c94784e42b9cdea317ec798bd
DarkSoldierBR <mateus.dias029@gmail.com>
2020-02-06 20:02:14 -0300

    fix layout

--- main.py
+++ main.py
@@ -1,52 +1,60 @@
 from tkinter import *
+
+root = Tk()
+
 class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
        
-        #Containers       
+        # #Principal
+        # containermain = Frame(master)
+        # containermain.pack ()
+
+        #Containers Seção 1      
         container1 = Frame(master,borderwidth=2, relief="groove")
-        container1.pack ()
+        container1.pack (fill=X,pady=10,padx=5)
 
 
-        container2 = Frame(container1)
-        container2.pack ()
+        container1_child1 = Frame(container1)
+        container1_child1.pack ()
 
 
-        container3 = Frame(container1)
-        container3.pack ()
+        container1_child2 = Frame(container1)
+        container1_child2.pack ()
 
-        ####
+        #Containers Seção 2
 
-        container4 = Frame(master,borderwidth=2, relief="groove")
-        container4.pack ()
+        container2 = Frame(master,height = 200,borderwidth=2, relief="groove")
+       # container2.place(relx=0.5, rely=0.5, anchor=CENTER)
+        container2.pack (fill=X,padx=5)
 
-        ####
+        #Containers Seção 3
 
         container5 = Frame(master)
-        container5.pack ()
+        container5.pack (pady=10)
 
         #Texts
-        text1 = Label(container2, text="Diretorios",)
+        text1 = Label(container1_child1, text="Diretorios",)
         text1.pack ()
 
-        text2 = Label(container2, text="Caminho do backup local",)
+        text2 = Label(container1_child1, text="Caminho do backup local",)
         text2.pack (side=LEFT)
 
-        text3 = Label(container3, text="Caminho pendrive",)
+        text3 = Label(container1_child2, text="Caminho pendrive",)
         text3.pack (side=LEFT)
 
         ####
 
-        text3 = Label(container4, text="Opções")
+        text3 = Label(container2, text="Opções")
         text3.pack ()
 
 
 
         #Entrys
-        localbackup = Entry(container2)
+        localbackup = Entry(container1_child1)
         localbackup.pack (side=RIGHT)
 
-        localpendrive = Entry(container3)
+        localpendrive = Entry(container1_child2)
         localpendrive.pack (side=RIGHT)
 
         #Radio Buttons
@@ -54,13 +62,13 @@ class Application(Frame):
         rb2 = 0
         rb3 = 0
 
-        radiobutton1= Radiobutton(container4,text="Excluir(arquivos exitentes)",variable=rb1, value=1)
+        radiobutton1= Radiobutton(container2,text="Excluir(arquivos exitentes)",variable=rb1, value=1)
         radiobutton1.pack ()
 
-        radiobutton2= Radiobutton(container4,text="Recursivo",variable=rb2, value=1)
+        radiobutton2= Radiobutton(container2,text="Recursivo",variable=rb2, value=1)
         radiobutton2.pack ()
 
-        radiobutton3= Radiobutton(container4,text="Atualizar(arquivos exitentes)",variable=rb3, value=1)
+        radiobutton3= Radiobutton(container2,text="Atualizar(arquivos exitentes)",variable=rb3, value=1)
         radiobutton3.pack ()
 
         #Buttons
@@ -68,7 +76,7 @@ class Application(Frame):
         buttonsalvar = Button(text="Salvar")
         buttonsalvar.pack ()
 
-root = Tk()
+
 
 window_height = 300
 window_width = 350
